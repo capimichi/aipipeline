3d24df88c74092d70b43fb6eb3620de482202a99
Michele Capichioni <capimichi@gmail.com>
2024-03-30 18:55:42 +0100

    ReadFileTask added, reads content of a file and outputs it as 'content'

---
+++ aipipeline/task/ReadFileTask.py
@@ -0,0 +1,21 @@
+from aipipeline.task.BaseTask import BaseTask
+
+
+class ReadFileTask(BaseTask):
+
+    file_path = None
+    content = None
+
+    def get_required_inputs(self):
+        return ['file_path']
+
+    def set_inputs(self, inputs):
+        self.file_path = inputs['file_path']
+
+    def run(self):
+        with open(self.file_path, 'r') as file:
+            self.content = file.read()
+        return self.content
+
+    def get_outputs(self):
+        return {'content': self.content}
