b36e951ed6def7f47ebe597b997c968b72ef10a8
Michele Capichioni <capimichi@gmail.com>
2024-03-31 20:33:26 +0200

    "Added support for using container instances and updated container instantiation method"

--- aipipeline.py
+++ aipipeline.py
@@ -5,10 +5,13 @@ import requests
 import re
 import subprocess
 
+from aipipeline.container.DefaultContainer import DefaultContainer
 from aipipeline.parser.PipelineParser import PipelineParser
 
 pipeline_path = sys.argv[1]
 
-pipeline_parser = PipelineParser()
+default_container = DefaultContainer.get_instance()
+
+pipeline_parser:PipelineParser = default_container.get(PipelineParser.__name__)
 
 pipeline = pipeline_parser.parse(pipeline_path)
