64c994c693553f56cd3d78f747e99518c5bfa1b1
Michele Capichioni <capimichi@gmail.com>
2024-03-31 23:13:46 +0200

    Fixed missing getter methods for inputs and outputs in Task class

--- aipipeline/model/Task.py
+++ aipipeline/model/Task.py
@@ -8,3 +8,9 @@ class Task(BaseModel):
     type: str
     inputs: Optional[Dict[str, Any]] = None
     outputs: Optional[Dict[str, Any]] = None
+
+    def get_inputs(self):
+        return self.inputs or {}
+
+    def get_outputs(self):
+        return self.outputs or {}
