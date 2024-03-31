4b096dcdbb1fc55d89d8ca4ff8fb3afebc7df0fe
Michele Capichioni <capimichi@gmail.com>
2024-03-31 23:13:57 +0200

    commit message: Added pipeline context to has_required_inputs method

--- aipipeline/parser/TaskParser.py
+++ aipipeline/parser/TaskParser.py
@@ -11,7 +11,7 @@ class TaskParser:
     def validate(self, task : Task, pipeline : Pipeline):
         task_instance = self.get_task_instance(task)
 
-        self.has_required_inputs(task_instance, task)
+        self.has_required_inputs(task_instance, task, pipeline)
 
 
     def get_task_instance(self, task : Task):
@@ -22,35 +22,39 @@ class TaskParser:
             self.instances[task_class] = instance
         return self.instances[task_class]
 
-    def has_required_inputs(self, task_instance, task: Task):
+    def has_required_inputs(self, task_instance, task: Task, pipeline : Pipeline):
         required_inputs = task_instance.get_required_inputs()
         existing_inputs = []
-        inputs = task.inputs
+        inputs = task.get_inputs()
 
         for required_input in required_inputs:
             if required_input in inputs:
                 existing_inputs.append(required_input)
 
         task_id = task.id
-        outputs = task.outputs
         missing_inputs = list(set(required_inputs) - set(existing_inputs))
         for missing_input in missing_inputs:
-            for output_key in outputs:
-                output = outputs[output_key]
-                output_list = []
-                if not isinstance(output, list):
-                    output_list.append(output)
-                else:
-                    output_list = output
+            loop_tasks = pipeline.tasks
+            for loop_task in loop_tasks:
+                loop_outputs = loop_task.get_outputs()
+                for output_key in loop_outputs:
+                    output = loop_outputs[output_key]
+                    output_list = []
+                    if not isinstance(output, list):
+                        output_list.append(output)
+                    else:
+                        output_list = output
 
-                for output in output_list:
-                    output_task_id = self.get_output_task_id(output)
-                    output_input = self.get_output_input(output)
-                    if output_task_id == task_id and output_input == missing_input:
-                        existing_inputs.append(missing_input)
+                    for output in output_list:
+                        output_task_id = self.get_output_task_id(output)
+                        output_input = self.get_output_input(output)
+                        if output_task_id == task_id and output_input == missing_input:
+                            existing_inputs.append(missing_input)
+
+        missing_inputs = list(set(required_inputs) - set(existing_inputs))
 
         if(len(existing_inputs) != len(required_inputs)):
-            raise Exception(f"Task {task_instance} is missing required inputs {required_inputs}")
+            raise Exception(f"Task {task_instance} is missing required inputs {missing_inputs}")
 
     def get_output_task_id(self, output: str):
         return output.split('.')[0]
