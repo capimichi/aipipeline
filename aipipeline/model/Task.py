e1d80e86c8dbfb1f7fc2ea087bf3daa8b2cd7ce7
Michele Capichioni <capimichi@gmail.com>
2024-04-01 00:42:01 +0200

    Multiple tasks allowed per pipeline step

--- aipipeline/model/Task.py
+++ aipipeline/model/Task.py
@@ -8,9 +8,13 @@ class Task(BaseModel):
     type: str
     inputs: Optional[Dict[str, Any]] = None
     outputs: Optional[Dict[str, Any]] = None
+    multiple: Optional[bool] = False
 
     def get_inputs(self):
         return self.inputs or {}
 
     def get_outputs(self):
         return self.outputs or {}
+
+    def is_multiple(self):
+        return self.multiple
